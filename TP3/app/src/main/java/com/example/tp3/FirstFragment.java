9096ab161ae936f0a6f37673b4019146c7f27589
HugoGellidaFac <hugo.gellida34@gmail.com>
2026-03-10 16:27:20 +0100

    tp3

---
+++ TP3/app/src/main/java/com/example/tp3/FirstFragment.java
@@ -0,0 +1,51 @@
+package com.example.tp3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.tp3.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.button.setOnClickListener(v -> {
+            String name = binding.name.getText().toString().trim();
+            String date = binding.date.getText().toString().trim();
+            String first_name = binding.firstName.getText().toString().trim();
+            String mail = binding.mail.getText().toString().trim();
+            String phone = binding.phone.getText().toString().trim();
+            if (!name.isEmpty() && !date.isEmpty() && !first_name.isEmpty() && !mail.isEmpty() && !phone.isEmpty()){
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
